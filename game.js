d4b7afe378ce6d4ed12ad9743a065343cecb4a85
Haley <h.n.merrill@cox.net>
2022-10-09 12:09:41 -0700

    Finished game and added minimal styles to html file

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const options = ["rock", "paper", "scissors"];
 const optionsAsString = options.join(', ');
+let playerWins = 0;
+let computerWins = 0;
 
 function getComputerChoice(){
     var choice = Math.floor(Math.random() * 100) % 3;
@@ -12,22 +14,43 @@ function playRound(playerSelection, computerSelection){
         result = "You selected: " + playerSelection + ". The computer selected: " + computerSelection + ". It's a Tie!";
     } else if(playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper"){
         result = "You selected: " + playerSelection + ". The computer selected: " + computerSelection + ". You Win!";
+        playerWins = playerWins + 1;
     } else {
         result = "You selected: " + playerSelection + ". The computer selected: " + computerSelection + ". The Computer Wins!";
+        computerWins = computerWins + 1;
     }
     return result;
-
 }
 
 function playGame(){
-    let playerSelection = prompt("Make your choice!  Type either: " + optionsAsString);
-    playerSelection = playerSelection.toLowerCase();
-    //Error Checking User Input
-    if(options.includes(playerSelection)){
-        console.log("Approved");
-    } else {
-        console.log("Denied");
-    }
-    let computerSelection = getComputerChoice();
+    var result;
+    var isApproved;
+    for(var i = 0; i < 5; i++){
+        isApproved = false;
+        let playerSelection = prompt("Make your choice!  Type either: " + optionsAsString);
+        playerSelection = playerSelection.toLowerCase();
 
+        //Error Checking User Input
+        while(!isApproved){
+            if(options.includes(playerSelection)){
+                isApproved = true;
+            } else {
+                playerSelection = prompt("Check your spelling, type either: " + optionsAsString);
+            }
+        }
+        
+        let computerSelection = getComputerChoice();
+        result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        console.log("Currently, you have won " + playerWins + " and the computer has won " + computerWins);
+        if(playerWins == 3 || computerWins == 3){
+            break;
+        }
+    }
+    if(playerWins > computerWins){
+        console.log("Congratulations, you win!");
+    }  
+    else{
+        console.log("Better luck next time, click Start Game to try again!");
+    }
 }
