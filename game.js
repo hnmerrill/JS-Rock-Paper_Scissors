d1a1b7d698c78a7f631fc52228dc558ef6f50d04
Haley <h.n.merrill@cox.net>
2022-10-09 11:46:48 -0700

    Updated playRound function and added Console usability

--- game.js
+++ game.js
@@ -1,14 +1,33 @@
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+const options = ["rock", "paper", "scissors"];
+const optionsAsString = options.join(', ');
 
 function getComputerChoice(){
-    return Math.floor(Math.random() * 100) % 3;
+    var choice = Math.floor(Math.random() * 100) % 3;
+    return options[choice];
 }
 
 function playRound(playerSelection, computerSelection){
-    return "Entered playround";
+    var result = "";
+    if(playerSelection == computerSelection){
+        result = "You selected: " + playerSelection + ". The computer selected: " + computerSelection + ". It's a Tie!";
+    } else if(playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper"){
+        result = "You selected: " + playerSelection + ". The computer selected: " + computerSelection + ". You Win!";
+    } else {
+        result = "You selected: " + playerSelection + ". The computer selected: " + computerSelection + ". The Computer Wins!";
+    }
+    return result;
+
 }
 
 function playGame(){
-    var resultText = document.getElementById('result').innerHTML = "testing";
+    let playerSelection = prompt("Make your choice!  Type either: " + optionsAsString);
+    playerSelection = playerSelection.toLowerCase();
+    //Error Checking User Input
+    if(options.includes(playerSelection)){
+        console.log("Approved");
+    } else {
+        console.log("Denied");
+    }
+    let computerSelection = getComputerChoice();
+
 }
